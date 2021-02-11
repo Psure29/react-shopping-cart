15d1b008bec42eb1445cb4748e01ef6ae756c2b1
Psure29 <payalradadiya29@gmail.com>
2021-02-11 12:06:30 -0500

    Animation and modal

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
 
 export default class Products extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            product: null,
+        };
+    }
+    openModal = (product) => {
+        this.setState({product});
+    };
+    closeModal = () =>{
+        this.setState({product: null});
+    };
     render() {
+        const { product } = this.setState;
         return (
             <div>
+                <Fade bottom cascade>
                 <ul className="products">
                 {this.props.products.map(product=>(
                     <li key={product._id}>
                         <div className="product">
-                            <a href={"#" + product._id}>
+                            <a href={"#" + product._id}
+                                onClick={() => this.openModal(product)}
+                            >
                                 <img src={product.image} alt={product.title}></img> 
                                 <p>
                                     {product.title}
@@ -24,7 +43,24 @@ export default class Products extends Component {
                     </li>
                 ))}
                 </ul>
+                </Fade>
+                {product && (
+                        <Modal isOpen={true} onRequestClose={this.closeModal}>
+                            <Zoom>
+                               <button className="close-modal" onClick={this.closeModal}>x</button>
+                                <div className="product-details">
+                                    <img src={product.image} alt={product.title}></img>
+                                    <div>
+                                        <p>
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </Zoom>
+                        </Modal>
+                    )
+                }
             </div>
-        )
+        );
     }
 }
